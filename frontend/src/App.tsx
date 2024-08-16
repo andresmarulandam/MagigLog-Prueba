de5141f6351e156ba5d5d14405a3ee6a54ce4b7e
Andres M <marulandaandres19@gmail.com>
2024-08-16 01:38:32 -0500

    Fix bug

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,4 @@
-const URL = import.meta.env.VITE_BACKEND_URL || 'http://localhost:3000';
+//const URL = import.meta.env.VITE_BACKEND_URL || 'http://localhost:3000';
 
 const App = () => {
   return (
