78c34895a5e6bdddb0f75d3c493053166dd3e15c
Andres M <marulandaandres19@gmail.com>
2024-08-16 01:26:23 -0500

    Probar cambios luego de despliegue

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,9 @@
+const URL = import.meta.env.VITE_BACKEND_URL || 'http://localhost:3000';
+
 const App = () => {
   return (
     <div>
-      <h1 className="text-4xl font-bold ">Helloooo</h1>
+      <h1 className="text-4xl font-bold ">Vamos Bien</h1>
     </div>
   );
 };
