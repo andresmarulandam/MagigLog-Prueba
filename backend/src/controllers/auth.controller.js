2495eabef240c26f096ea043b4b8ea2089e2856b
Andres M <marulandaandres19@gmail.com>
2024-08-16 11:00:28 -0500

    Add login and logout routes and controllers

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -34,6 +34,42 @@ export const register = async (req, res) => {
   }
 };
 
-export const login = (req, res) => {
-  res.send('login');
+export const login = async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    const vendedorEncontrado = await Vendedor.findOne({ email });
+
+    if (!vendedorEncontrado)
+      return res.status(400).json({ message: 'Vendedor not found' });
+
+    const isMatch = await bcrypt.compare(password, vendedorEncontrado.password);
+
+    if (!isMatch)
+      return res.status(400).json({ message: 'Invalid credentials' });
+
+    const token = await createAccessToken({ id: vendedorEncontrado._id });
+
+    res.cookie('token', token, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === 'production',
+    });
+
+    res.json({
+      id: vendedorEncontrado._id,
+      email: vendedorEncontrado.email,
+      password: vendedorEncontrado.password,
+      createdAt: vendedorEncontrado.createdAt,
+      updatedAt: vendedorEncontrado.updatedAt,
+    });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+export const logout = async (req, res) => {
+  res.cookie('token', '', {
+    expires: new Date(0),
+  });
+  return res.sendStatus(200);
 };
